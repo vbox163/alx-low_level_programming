3b54dc1f101f270db9942e720b6a3aa7e011836e
vbox163 <olowe.wole@gmail.com>
2023-03-30 06:35:46 -0700

    commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 			return (NULL);
 
 		temp->str = strdup(str);
-		temp->len = _strlen(str);
+		temp->len = strlen(str);
 		temp->next = *head;
 
 		*head = temp;
