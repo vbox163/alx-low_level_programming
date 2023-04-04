1fe68b8301cfba800aa13434c7780fda5aee008c
vbox163 <olowe.wole@gmail.com>
2023-04-04 06:20:21 -0700

    commitment 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,4 +1,3 @@
-
 #include "lists.h"
 
 /**
@@ -7,26 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	/* Declare a temporary node pointer */
 	listint_t *temp;
 
-	/* Check if the input pointer is NULL */
 	if (head == NULL)
 		return;
 
-	/* Loop through the list while there are nodes to be freed */
 	while (*head)
 	{
-		/* Store the next node in the temporary pointer */
 		temp = (*head)->next;
-		
-		/* Free the current node */
 		free(*head);
-		
-		/* Update the input pointer to point to the next node */
 		*head = temp;
 	}
 
-	/* Set the input pointer to NULL after all nodes are freed */
 	*head = NULL;
 }
