063a63a48bf444c745592b8f783dd17d26e5d435
vbox163 <olowe.wole@gmail.com>
2023-04-03 23:48:00 -0700

    re-comit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,7 +10,7 @@ listint_len - returns the number of elements in a linked list
 
 Return: number of nodes in the linked list
 */
-size_t listint_len(const listint_t h)
+size_t listint_len(const listint_t *h)
 {
 size_t num = 0; / initialize a counter variable */
 
